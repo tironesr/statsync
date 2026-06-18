3eb5a0f5c00c4078003f4f55ad243a2dbbbb7fcf
tironesr <tironesr@gmail.com>
2026-06-18 13:24:21 -0700

    Add Access-Control-Allow-Private-Network header to fix Mac WebKit connection issues

--- statsync/R/export.R
+++ statsync/R/export.R
@@ -118,6 +118,7 @@ sync_serve <- function(project_name = "StatSync Project", ..., port = 8877,
         "Access-Control-Allow-Origin" = "*",
         "Access-Control-Allow-Methods" = "GET, OPTIONS",
         "Access-Control-Allow-Headers" = "Content-Type",
+        "Access-Control-Allow-Private-Network" = "true",
         "Content-Type" = "application/json"
       )
       
